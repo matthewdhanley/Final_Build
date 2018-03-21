6c32663dac8a124c0dd48fb9c5f95b9b26870eec
matthewdhanley <mattdhanley@gmail.com>
2018-03-21 22:22:36 +0000

    added apriltags submodule

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/robot/Final_Build/src
-# Build directory: /home/robot/Final_Build/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
-subdirs(bot/apriltags_ros/apriltags)
-subdirs(bot/apriltags_ros/apriltags_ros)
