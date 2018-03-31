733ea3b3df0c39bba0361560d9eba4d3b90b0cbb
matthewdhanley <maha2895@colorado.edu>
2018-03-31 12:09:57 -0600

    fixed launch file

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/robot/Final_Build/src
+# Build directory: /home/robot/Final_Build/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(bot/apriltags_ros/apriltags)
+subdirs(bot/apriltags_ros/apriltags_ros)
+subdirs(bot/usb_cam)
---
+++ build/bot/apriltags_ros/apriltags_ros/catkin_generated/apriltags_ros-msg-extras.cmake.develspace.in
@@ -0,0 +1,2 @@
+set(apriltags_ros_MESSAGE_FILES "/home/robot/Final_Build/src/bot/apriltags_ros/apriltags_ros/msg/AprilTagDetection.msg;/home/robot/Final_Build/src/bot/apriltags_ros/apriltags_ros/msg/AprilTagDetectionArray.msg")
+set(apriltags_ros_SERVICE_FILES "")
