9f3c601215f62e2f8bd265f1c0d4cbe0a9463ba7
matthewdhanley <maha2895@colorado.edu>
2018-03-31 12:11:50 -0600

    fixed

--- build/bot/usb_cam/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/robot/Final_Build/src/bot/usb_cam/include".split(';') if "/home/robot/Final_Build/src/bot/usb_cam/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-lusb_cam".split(';') if "-lusb_cam" != "" else []
-PROJECT_NAME = "usb_cam"
-PROJECT_SPACE_DIR = "/home/robot/Final_Build/devel"
-PROJECT_VERSION = "0.3.6"
